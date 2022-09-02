11cc456e78df86d53f5deabb03cab00b01b3bc18
Ray McDermott <ray@mcdermott.be>
2022-09-03 00:28:42 +0200

    unset others

--- public/cljs/api_client.cljs
+++ public/cljs/api_client.cljs
@@ -7,25 +7,26 @@
 (defonce state (r/atom nil))
 
 (defn post-simple
-  [tag unset-tags]
+  [tag unset-tags unset-others]
   (-> (p/let [options (clj->js {:method  "POST"
                                 :headers {:Content-Type "application/json"}
                                 :body    (-> (clj->js {:id (str (random-uuid) "-" (name tag))})
                                              (js/JSON.stringify))})
               result (js/fetch "http://localhost:3000/api/simple" options)
               body (p/-> result .json .-body js/JSON.parse (js->clj :keywordize-keys true))]
-        (swap! state (fn [m] (-> m
-                                 (assoc tag body)
-                                 (#(apply dissoc % unset-tags))))))
+        (if unset-others
+          (reset! state (hash-map tag body))
+          (swap! state (fn [m] (-> (assoc m tag body)
+                                   (#(apply dissoc % unset-tags)))))))
       (p/catch (fn [error]
                  (js/console.log :error error)))))
 
 (defn fn-request
-  [label set-tag & {:keys [unset-tags]
-                    or    []}]
+  [label set-tag & {:keys [unset-tags unset-others]
+                    :or   {unset-tags [] unset-others false}}]
   [:div.col-xs-2
    [:p [:button.btn.btn-info
-        {:on-click #(post-simple set-tag unset-tags)} label]]
+        {:on-click #(post-simple set-tag unset-tags unset-others)} label]]
    (when-let [data (get @state set-tag)]
      [:div (str "Result: " data)])])
 
@@ -35,7 +36,7 @@
    [:div.row
     [:h1 "API Client"]]
    [:div.row
-    [fn-request "Request Y" :tag-y]
+    [fn-request "Request Y" :tag-y :unset-others true]
     [fn-request "Request Z" :tag-z :unset-tags [:tag-a]]
     [fn-request "Request A" :tag-a :unset-tags [:tag-z :tag-b]]
     [fn-request "Request B" :tag-b :unset-tags [:tag-a]]]])
